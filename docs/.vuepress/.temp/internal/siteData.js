712ae40a5ab4c8f3f444392ad6797acd73d2e3d5
flycode <1821024942@qq.com>
2024-07-17 17:46:06 +0800

    feat:update

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/webFaceBook/\",\"lang\":\"zh-CN\",\"title\":\"前端面试宝典\",\"description\":\"一个求职的前端小白总结的面试题\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
--- docs/.vuepress/.temp/pages/htmlMd/html.html.js
+++ docs/.vuepress/.temp/pages/htmlMd/html.html.js
@@ -1,16 +1,3 @@
 import comp from "E:/code/webFaceBook/docs/.vuepress/.temp/pages/htmlMd/html.html.vue"
-const data = JSON.parse("{\"path\":\"/htmlMd/html.html\",\"title\":\"HTML\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1721200495000,\"contributors\":[{\"name\":\"flycode\",\"email\":\"1821024942@qq.com\",\"commits\":1}]},\"filePathRelative\":\"htmlMd/html.md\"}")
+const data = JSON.parse("{\"path\":\"/htmlMd/html.html\",\"title\":\"HTML\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"htmlMd/html.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
